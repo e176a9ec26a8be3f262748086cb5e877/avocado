84230b65ae29004c56779d754cea7e2fdcf0be84
northbot <154997465+northbot@users.noreply.github.com>
2024-10-04 09:23:25 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,13 +4,13 @@ int main() {
     const char *home = getenv("HOME");
     char sdk_path[512];
     const char *java_home = "/usr/lib/jvm/java-17-openjdk-amd64";
-    const char *emulator_path = "/home/user/Android/Sdk/emulator";
+    const char *emulator_path = "/home/user/Android/Sdk/emulator/emulator";
+    const char *adb_path = "/home/user/Android/Sdk/platform-tools/adb";
     const char *current_path = getenv("PATH");
     char new_path[4096];
     char command[512];
     char manual_command[256];
     char emulator_command[1024];
-
 
     if (home == NULL) {
         fprintf(stderr, "Erro: Variável de ambiente HOME não encontrada.\n");
@@ -18,9 +18,11 @@ int main() {
     }
 
     snprintf(sdk_path, sizeof(sdk_path), "%s/Android/Sdk/cmdline-tools/latest/bin", home);
+
     setenv("JAVA_HOME", java_home, 1);
+
     snprintf(new_path, sizeof(new_path), "%s/bin:%s:%s:%s/platform-tools:%s/tools/bin:%s:%s",
-             java_home, sdk_path, sdk_path, sdk_path, sdk_path, sdk_path, current_path);
+             java_home, sdk_path, sdk_path, "/home/user/Android/Sdk", "/home/user/Android/Sdk", "/home/user/Android/Sdk", current_path);
     setenv("PATH", new_path, 1);
 
     while (1) {
@@ -46,13 +48,22 @@ int main() {
         }
 
         if (strncmp(command, "emulator", 8) == 0) {
-            snprintf(emulator_command, sizeof(emulator_command), "%s/%s", emulator_path, command);
+            snprintf(emulator_command, sizeof(emulator_command), "%s %s", emulator_path, command + 9);
 
             int status = system(emulator_command);
             if (status == -1) {
                 perror("Erro ao executar comando do emulator");
             }
-        } else {
+        }
+        else if (strncmp(command, "adb", 3) == 0) {
+            snprintf(emulator_command, sizeof(emulator_command), "%s %s", adb_path, command + 4);
+
+            int status = system(emulator_command);
+            if (status == -1) {
+                perror("Erro ao executar comando do adb");
+            }
+        }
+        else {
             int status = system(command);
             if (status == -1) {
                 perror("Erro ao executar comando");
